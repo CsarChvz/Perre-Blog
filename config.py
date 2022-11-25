2946ec4d36a4303ec1f0ff8f9df15f88b61f1019
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-11-24 20:59:07 -0600

    asdf

---
+++ config.py
@@ -0,0 +1,55 @@
+import os
+
+class Config:
+ 
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def __init__(app):
+        pass
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'gwxelrbjzrhmqtnh'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/perre"
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/perre"
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/perre"
+
+config = {
+'development': DevelopmentConfig,
+'testing': TestingConfig,
+'production': ProductionConfig,
+'default': DevelopmentConfig
+}
